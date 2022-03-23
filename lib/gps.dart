b16690d7ec6b3d14cb85d8d774b1ec95c7035ccb
Debapriyo Paul <debapriyopaul.dp@gmail.com>
2022-03-23 23:24:46 +0530

    location update

--- lib/gps.dart
+++ lib/gps.dart
@@ -11,11 +11,14 @@ class Gps extends StatefulWidget {
 class _GpsState extends State<Gps> {
   // LocationData _locationData=LocationData();
   Location location = Location();
+  String lat = "k";
+  String long = "s";
 
   @override
   void initState() {
     super.initState();
     mylocation();
+    setState(() {});
   }
 
   Future<void> mylocation() async {
@@ -41,13 +44,39 @@ class _GpsState extends State<Gps> {
 
     setState(() async {
       _locationData = await location.getLocation();
+      lat = _locationData.latitude.toString();
+      long = _locationData.longitude.toString();
     });
   }
 
   @override
-  Future<Widget> build(BuildContext context) async {
-    return Container(
-      child: Text((await location.getLocation()).latitude.toString()),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text("Latitude : "),
+            Text(
+              lat,
+            ),
+            const Text("Longitude : "),
+            Text(
+              long,
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  mylocation();
+                });
+              },
+              child: const Text(
+                "GET",
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
